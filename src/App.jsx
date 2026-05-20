f49af7c89d70e2597fb11e976aca9dc6fb3f62e6
Elayaraman <elayaraman007@gmail.com>
2026-05-21 01:24:38 +0530

    refining

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filterData, setFilterData] = useState([]);
 
-  const [data,setData] = useState([]);
-  const [search,setSearch] = useState("");
-  let [filterData, setFilterData] = useState([])
-
-
-  useEffect(()=>{
-    async function getData(params) {
-      const response = await (await fetch('https://jsonplaceholder.typicode.com/users')).json()
+  useEffect(() => {
+    async function getData() {
+      const response = await (
+        await fetch("https://jsonplaceholder.typicode.com/users")
+      ).json();
       setData(response);
     }
     getData();
-  },[]);
+  }, []);
 
-  useEffect(()=>{
-    console.log(search);
-    setFilterData(data.filter((user)=> user.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())));
-    console.log(filterData)
-  },[search])
+  useEffect(() => {
+    
+    const timer = setTimeout(()=>{
+      console.log(search);
+      setFilterData(
+        data.filter((user) =>
+          user.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+        ),
+      )},
+      150,
+    );
+    console.log(filterData);
+    return ()=> clearTimeout(timer);
+  }, [search]);
 
   return (
     <div>
-      <input type="text" value={search} onChange={e => {setSearch(e.target.value)}} />
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
       <div>
-        {(search ? filterData :data).map((item)=>{
-          return <p key={item.id}>{item.name}</p>
+        {(search ? filterData : data).map((item) => {
+          return <p key={item.id}>{item.name}</p>;
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
