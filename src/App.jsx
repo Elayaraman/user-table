4845f10cd500a5c8fd33b8c14a11a5f3f3841344
Elayaraman <elayaraman007@gmail.com>
2026-05-21 01:37:06 +0530

    11 mins

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
-
 function App() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-  const [filterData, setFilterData] = useState([]);
+  const [sort, setSort] = useState("asc");
 
   useEffect(() => {
     async function getData() {
@@ -11,36 +10,37 @@ function App() {
         await fetch("https://jsonplaceholder.typicode.com/users")
       ).json();
       setData(response);
+      return;
     }
     getData();
   }, []);
 
-  useEffect(() => {
-    
-    const timer = setTimeout(()=>{
-      console.log(search);
-      setFilterData(
-        data.filter((user) =>
-          user.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
-        ),
-      )},
-      150,
-    );
-    console.log(filterData);
-    return ()=> clearTimeout(timer);
-  }, [search]);
+  const filteredData = data
+    .filter((user) => user.name.toLowerCase().includes(search.toLowerCase()))
+    .sort((a, b) => {
+      return sort === "asc"
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name);
+    });
 
   return (
     <div>
       <input
         type="text"
         value={search}
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
+        onChange={(e) => setSearch(e.target.value)}
       />
+      <select
+        name="sort"
+        id=""
+        value={sort}
+        onChange={(e) => setSort(e.target.value)}
+      >
+        <option value="asc">A-Z</option>
+        <option value="desc">Z-A</option>
+      </select>
       <div>
-        {(search ? filterData : data).map((item) => {
+        {filteredData.map((item) => {
           return <p key={item.id}>{item.name}</p>;
         })}
       </div>
